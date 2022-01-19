aab89726a4fece687fdb2e59742a31b25654723c
AVP <aakpat6@gmail.com>
2022-01-19 07:25:00 -0800

    iris rev 6 init

---
+++ keyboards/keebio/iris/keymaps/avp/config.h
@@ -0,0 +1,26 @@
+/*
+Copyright 2017 Danny Nguyen <danny@keeb.io>
+
+This program is free software: you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation, either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program.  If not, see <http://www.gnu.org/licenses/>.
+*/
+
+#pragma once
+
+// #define USE_I2C
+#define EE_HANDS
+#define MASTER_LEFT
+
+#define RGB_MATRIX_STARTUP_HUE 85                             // Sets the default hue value, if none has been set
+#define RGB_MATRIX_STARTUP_SAT 255                            // Sets the default saturation value, if none has been set
+#define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS  // Sets the default brightness value, if none has been set
