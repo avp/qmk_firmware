32307ed2c63a23a24739c550f42398bf31e4a7d8
AVP <aakpat6@gmail.com>
2022-01-19 16:59:44 -0800

    max brightness

--- keyboards/keebio/iris/keymaps/avp/config.h
+++ keyboards/keebio/iris/keymaps/avp/config.h
@@ -21,6 +21,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 #define EE_HANDS
 #define MASTER_LEFT
 
+#undef RGB_MATRIX_MAXIMUM_BRIGHTNESS
+#define RGB_MATRIX_MAXIMUM_BRIGHTNESS 100
 #define RGB_MATRIX_STARTUP_HUE 85                             // Sets the default hue value, if none has been set
 #define RGB_MATRIX_STARTUP_SAT 255                            // Sets the default saturation value, if none has been set
 #define RGB_MATRIX_STARTUP_VAL RGB_MATRIX_MAXIMUM_BRIGHTNESS  // Sets the default brightness value, if none has been set
